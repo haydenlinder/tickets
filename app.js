dd94ce3e302d8c4d8687e1be32473362a613ccf3
jjohnston <jpjohnston@ucdavis.edu>
2020-03-16 16:13:12 -0700

    complete half-auth

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require('express');
+const app = express()
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const passport = require('passport')
-
 const db = require('./config/keys').mongoURI;
-const users = require("./routes/api/users")
-const tickets = require("./routes/api/tickets")
+const users = require("./routes/api/users");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(passport.initialize())
-require('./config/passport')(passport);
-
-app.use("/api/users", users)    
-app.use("/api/tickets", tickets)
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+app.get("/", (req, res) => {
+    const user = new User ({
+        first_name: "jim2",
+        last_name: "jones",
+        email: "jim@catsforhumanity.org",
+        password: "jimisgreat123"
+    })
+    user.save()
+    res.send("Hello World!")
+})
+
+app.use("/api/users", users)   
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});
+
+
--- models/User.js
+++ models/User.js
@@ -2,18 +2,27 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    first_name: {
+        type: String, 
+        required: true
+    },
+    last_name: {
+        type: String, 
+        required: true
+    },
     email: {
-        type: String,
+        type: String, 
         required: true
     },
     password: {
         type: String,
         required: true
     },
-    createdAt: {
+    created_at: {
         type: Date,
         default: Date.now
     }
 })
 
-User = mongoose.model('User', UserSchema)
+const User = mongoose.model('users', UserSchema);
+module.exports = User;
