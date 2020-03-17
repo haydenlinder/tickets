9a1f3fcfe4f0749b6454d24046052364cdb6049c
jjohnston <jpjohnston@ucdavis.edu>
2020-03-16 18:20:27 -0700

    message

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-const users = require("./routes/api/users");
 const User = require("./models/User");
 const bodyParser = require("body-parser");
-
 const users = require("./routes/api/users")
 const tickets = require("./routes/api/tickets")
 const tags = require('./routes/api/tags')
+const passport = require('passport');
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -45,8 +43,6 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/users", users)   
-
-const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => {console.log(`Server is running on port ${port}`)});
