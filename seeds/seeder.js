8793997f43df50d64b5a0f94a1b388a753bf52e6
marstrong <marshallstrong123@gmail.com>
2020-03-18 14:00:56 -0700

    commented out the seeder file

--- seeds/seeder.js
+++ seeds/seeder.js
@@ -1,54 +1,54 @@
-// import seeder from "mongoose-seed";
-const { seeder } = require('mongo-seeding');
+// // import seeder from "mongoose-seed";
+// const { seeder } = require('mongo-seeding');
 
-// const db = require('./config/keys').mongoURI;
-const config = {
-    database: {
-        host: '127.0.0.1',
-        port: 27017,
-        name: 'mydatabase',
-    },
-    dropDatabase: true,
-};
+// // const db = require('./config/keys').mongoURI;
+// const config = {
+//     database: {
+//         host: '127.0.0.1',
+//         port: 27017,
+//         name: 'mydatabase',
+//     },
+//     dropDatabase: true,
+// };
 
-// const organizationModel = require('../models/organization');
-const organizationSeeds = require('./organizations');
-// const tagModel = require('../models/tag');
-const tagSeeds = require('./tags');
-// const ticketModel = require('../models/ticket');
-const ticketSeeds = require('./tickets');
-// const userModel = require('../models/user');
-const userSeeds = require('./users');
+// // const organizationModel = require('../models/organization');
+// const organizationSeeds = require('./organizations');
+// // const tagModel = require('../models/tag');
+// const tagSeeds = require('./tags');
+// // const ticketModel = require('../models/ticket');
+// const ticketSeeds = require('./tickets');
+// // const userModel = require('../models/user');
+// const userSeeds = require('./users');
 
 
-const seeds = [
-    organizationSeeds,
-    // tagSeeds,
-    // ticketSeeds,
-    userSeeds
-];
+// const seeds = [
+//     organizationSeeds,
+//     // tagSeeds,
+//     // ticketSeeds,
+//     userSeeds
+// ];
 
-seeder.connect(db, () => {
-    seeder.loadModels([
-        '../models/organization',
-        // '../models/tag',
-        // '../models/ticket',
-        '../models/user',
-    ]);
-    seeder.clearModels([
-        'organization',
-        // 'tag',
-        // 'ticket',
-        'user',
-    ], () => {
-        seeder.populateModels(seeds, (err, done) => {
-            if (err) {
-                return console.log("seeding error", err);
-            }
-            if (done) {
-                return console.log("seeding completed", done);
-            }
-            seeder.disconnect()
-        });
-    });
-});
+// seeder.connect(db, () => {
+//     seeder.loadModels([
+//         '../models/organization',
+//         // '../models/tag',
+//         // '../models/ticket',
+//         '../models/user',
+//     ]);
+//     seeder.clearModels([
+//         'organization',
+//         // 'tag',
+//         // 'ticket',
+//         'user',
+//     ], () => {
+//         seeder.populateModels(seeds, (err, done) => {
+//             if (err) {
+//                 return console.log("seeding error", err);
+//             }
+//             if (done) {
+//                 return console.log("seeding completed", done);
+//             }
+//             seeder.disconnect()
+//         });
+//     });
+// });
