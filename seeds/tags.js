6718943762ef8dcd7538d3f8a621dbe99d22c74e
marstrong <marshallstrong123@gmail.com>
2020-03-18 13:51:47 -0700

    changed seeds to doublequotes to match mongoDB gui

--- seeds/tags.js
+++ seeds/tags.js
@@ -1,25 +1,25 @@
 const tagSeeds = {
-    // 'model': 'Organization',
-    // 'documents': [
+    // "model": "Organization",
+    // "documents": [
     //     {
-    //         'handle': 'cats4humanity.org',
-    //         'name': 'Cats for Humanity',
-    //         'motto': 'a cat in every household!'
+    //         "handle": "cats4humanity.org",
+    //         "name": "Cats for Humanity",
+    //         "motto": "a cat in every household!"
     //     },
     //     {
-    //         'handle': 'address',
-    //         'name': 'ACME',
-    //         'motto': "if you take your cues from error messages, this is the company for you"
+    //         "handle": "address",
+    //         "name": "ACME",
+    //         "motto": "if you take your cues from error messages, this is the company for you"
     //     },
     //     {
-    //         'handle': 'gmail.com',
-    //         'name': 'Google',
-    //         'motto': "if you have a gmail account, you work at Google!"
+    //         "handle": "gmail.com",
+    //         "name": "Google",
+    //         "motto": "if you have a gmail account, you work at Google!"
     //     },
     //     {
-    //         'handle': 'appacademy.io',
-    //         'name': 'appAcademy',
-    //         'motto': "appAcademy: if you liked your old life, you wouldn't be here. "
+    //         "handle": "appacademy.io",
+    //         "name": "appAcademy",
+    //         "motto": "appAcademy: if you liked your old life, you wouldn"t be here. "
     //     },
     // ]
 };
--- seeds/users.js
+++ seeds/users.js
@@ -1,75 +1,75 @@
 const userSeeds = {
-    'model': 'User',
-    'documents': [
+    "model": "User",
+    "documents": [
         {
-            'firstName': 'Hayden',
-            'lastName': 'Linder',
-            'email': 'hayden@cats4humanity.org',
-            'organization': 'cats4humanity.org',
-            'password': 'password'
+            "firstName": "Hayden",
+            "lastName": "Linder",
+            "email": "hayden@cats4humanity.org",
+            "organization": "cats4humanity.org",
+            "password": "password"
         },
         {
-            'firstName': 'Brad',
-            'lastName': 'Nelson',
-            'email': 'brad@cats4humanity.org',
-            'organization': 'cats4humanity.org',
-            'password': 'password'
+            "firstName": "Brad",
+            "lastName": "Nelson",
+            "email": "brad@cats4humanity.org",
+            "organization": "cats4humanity.org",
+            "password": "password"
         },
         {
-            'firstName': 'Joe',
-            'lastName': 'Johnston',
-            'email': 'joe@cats4humanity.org',
-            'organization': 'cats4humanity.org',
-            'password': 'password'
+            "firstName": "Joe",
+            "lastName": "Johnston",
+            "email": "joe@cats4humanity.org",
+            "organization": "cats4humanity.org",
+            "password": "password"
         },
         {
-            'firstName': 'Marshall',
-            'lastName': 'Strong',
-            'email': 'marshall@cats4humanity.org',
-            'organization': 'cats4humanity.org',
-            'password': 'password'
+            "firstName": "Marshall",
+            "lastName": "Strong",
+            "email": "marshall@cats4humanity.org",
+            "organization": "cats4humanity.org",
+            "password": "password"
         }, 
         {
-            'firstName': 'Demo',
-            'lastName': 'User',
-            'email': 'user@cats4humanity.org',
-            'organization': 'cats4humanity.org',
-            'password': 'password'
+            "firstName": "Demo",
+            "lastName": "User",
+            "email": "user@cats4humanity.org",
+            "organization": "cats4humanity.org",
+            "password": "password"
         },
         {
-            'firstName': 'ACME',
-            'lastName': 'User',
-            'email': 'email@address',
-            'organization': 'address',
-            'password': 'password'
+            "firstName": "ACME",
+            "lastName": "User",
+            "email": "email@address",
+            "organization": "address",
+            "password": "password"
         },
         {
-            'firstName': 'gmail',
-            'lastName': 'User',
-            'email': 'user@gmail.com',
-            'organization': 'gmail.com',
-            'password': 'password'
+            "firstName": "gmail",
+            "lastName": "User",
+            "email": "user@gmail.com",
+            "organization": "gmail.com",
+            "password": "password"
         },
         {
-            'firstName': 'Carlos',
-            'lastName': 'Garcia',
-            'email': 'carlos@appacademy.io',
-            'organization': 'appacademy.io',
-            'password': 'password'
+            "firstName": "Carlos",
+            "lastName": "Garcia",
+            "email": "carlos@appacademy.io",
+            "organization": "appacademy.io",
+            "password": "password"
         },
         {
-            'firstName': 'Julia',
-            'lastName': 'Wang',
-            'email': 'julia@appacademy.io',
-            'organization': 'appacademy.io',
-            'password': 'password'
+            "firstName": "Julia",
+            "lastName": "Wang",
+            "email": "julia@appacademy.io",
+            "organization": "appacademy.io",
+            "password": "password"
         },
         {
-            'firstName': 'Mike',
-            'lastName': 'Madsen',
-            'email': 'mike@appacademy.io',
-            'organization': 'appacademy.io',
-            'password': 'password'
+            "firstName": "Mike",
+            "lastName": "Madsen",
+            "email": "mike@appacademy.io",
+            "organization": "appacademy.io",
+            "password": "password"
         },
     ]
 };
