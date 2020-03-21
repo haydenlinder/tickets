5f51e902ca17a4199f048aa61a5081a912bc95f9
jjohnston <jpjohnston@ucdavis.edu>
2020-03-20 18:45:57 -0700

    another commit

--- frontend/src/components/tickets/activity_index_item.jsx
+++ frontend/src/components/tickets/activity_index_item.jsx
@@ -9,42 +9,42 @@ class ActivityIndexItem extends React.Component{
     }
 
     render() {
-        const {ticket} = this.props
+        // const {ticket} = this.props
 
-        let updateInfo = [];
-        let updateObj = {
-            time: "",
-            firstName: "",
-            lastName: ""
-        };
-           for(let i = 0; i < timeUpdate.length; i += 1) {
-                    let time = timeUpdate[i];
-                    updateObj[time] = time;
-                    let user = ticket.updatedBy[i];
-                    updateObj[firstName] = user.firstName;
-                    updateObj[lastName] = user.LastName;
+        // let updateInfo = [];
+        // let updateObj = {
+        //     time: "",
+        //     firstName: "",
+        //     lastName: ""
+        // };
+        //    for(let i = 0; i < timeUpdate.length; i += 1) {
+        //             let time = timeUpdate[i];
+        //             updateObj[time] = time;
+        //             let user = ticket.updatedBy[i];
+        //             updateObj[firstName] = user.firstName;
+        //             updateObj[lastName] = user.LastName;
 
-                    updateInfo.push(updateObj)
-           }
+        //             updateInfo.push(updateObj)
+        //    }
 
-        let updates = updateInfo.map(info =>  
-            <div>
-                <span> {info.time} </span>
-                <Link to={`/user/${user.id}`} > {info.firstName} {info.lastName}</Link> 
-            </div> 
-            )
+        // let updates = updateInfo.map(info =>  
+        //     <div>
+        //         <span> {info.time} </span>
+        //         <Link to={`/user/${user.id}`} > {info.firstName} {info.lastName}</Link> 
+        //     </div> 
+        //     )
 
 
-        ticketComments = ticket.comments.map(comment => <p> {comment.body} </p> )
-        userCommentLink = ticket.comments.author.map(user => <Link to={`/user/${user.id}`}> {user.firstName} {user.lastName} </Link>)
+        // ticketComments = ticket.comments.map(comment => <p> {comment.body} </p> )
+        // userCommentLink = ticket.comments.author.map(user => <Link to={`/user/${user.id}`}> {user.firstName} {user.lastName} </Link>)
 
         return (
           <div>
             <label> Updates:
-                    {updates}
+                    {/* {updates} */}
             </label>
             <label> Comments:
-                <p> {ticketComments} {userCommentLink} </p>
+                {/* <p> {ticketComments} {userCommentLink} </p> */}
             </label>
 
             <CommentForm />
