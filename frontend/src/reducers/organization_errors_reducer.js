4c7cdadec14a1d64ca0b346b3b0eef555a434703
marstrong <marshallstrong123@gmail.com>
2020-04-05 13:34:44 -0700

    organization_errors_reducer

--- frontend/src/reducers/organization_errors_reducer.js
+++ frontend/src/reducers/organization_errors_reducer.js
@@ -1,6 +1,19 @@
-import {} from '../actions/organization_actions';
+import { RECEIVE_ORG_ERRORS, CLEAR_ORG_ERRORS }
+  from '../actions/organization_actions';
 
 
 const organizationErrorsReducer = (defaultState = {}, action) => {
-  Object.freeze(defaultState)
+  Object.freeze(defaultState);
+
+  switch (action.type) {
+    case RECEIVE_ORG_ERRORS:
+      return action.errors;
+    case CLEAR_ORG_ERRORS:
+      return {};
+    default:
+      return defaultState;
+  }
 };
+
+
+export default organizationErrorsReducer;
