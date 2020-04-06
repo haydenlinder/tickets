e46d72c44b6bde5e7394cd44acafd2a9b8d365b6
marstrong <marshallstrong123@gmail.com>
2020-04-05 17:07:00 -0700

    added regex_utils file

---
+++ frontend/src/util/regex_util.js
@@ -0,0 +1,5 @@
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_special_characters
+
+const escapeRegexCharacters = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+export default escapeRegexCharacters;
